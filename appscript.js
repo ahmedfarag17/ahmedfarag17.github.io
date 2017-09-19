c6a6c95210a483204afde7cd08ab2cbb6d61ad36
ahmedfarag17 <ahmedfarag_jackk@yahoo.com>
2017-09-18 21:13:42 -0400

    Update appscript.js

--- appscript.js
+++ appscript.js
@@ -2,6 +2,7 @@
 function fillIn(identifier){
 
   switch (identifier) {
+    case "ahs": window.location = "ahs.html"; return;
     case "bp": window.location = "bloodpressure.html"; return;
     case "diabetes": window.location = "diabetes.html";return;
     case "med": window.location = "medication.html";return;
